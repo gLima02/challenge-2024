a3b01308b7fc54f72a00522dacc7204eed7e8f99
gLima <guilherme.l.oliveira2011@gmail.com>
2024-08-18 23:23:44 -0300

    FIX: titulo

--- src/app/components/portal/portal.component.ts
+++ src/app/components/portal/portal.component.ts
@@ -14,11 +14,11 @@ export class PortalComponent {
 
   // Lista de módulos
   modules = [
-    { name: 'Módulo 1 Básico I', progress: 100 },
-    { name: 'Módulo 2 Básico II', progress: 33 },
-    { name: 'Módulo 3 Intermediário I', progress: 0 },
-    { name: 'Módulo 4 Intermediário II', progress: 0 },
-    { name: 'Módulo 5 Avançado', progress: 0 }
+    { name: 'Módulo 1 | Básico I', progress: 100 },
+    { name: 'Módulo 2 | Básico II', progress: 33 },
+    { name: 'Módulo 3 | Intermediário I', progress: 0 },
+    { name: 'Módulo 4 | Intermediário II', progress: 0 },
+    { name: 'Módulo 5 | Avançado', progress: 0 }
   ];
 
   // Alterna a visibilidade dos módulos
