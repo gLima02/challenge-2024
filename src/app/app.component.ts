131ba2a6867f1fd925264520abc16e9af061a753
gLima <guilherme.l.oliveira2011@gmail.com>
2024-08-18 22:42:08 -0300

    ADD: terceira tela / display cursos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TelaLoginComponent } from './components/tela-login/tela-login.component';
-
+import { PortalComponent } from './components/portal/portal.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TelaLoginComponent],
+  imports: [RouterOutlet, TelaLoginComponent, PortalComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/portal/portal.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './portal.component.html',
+  styleUrls: ['./portal.component.css'] // Corrigido de 'styleUrl' para 'styleUrls'
+})
+export class PortalComponent {
+  // Controle da visibilidade dos módulos
+  showModules: boolean = false;
+
+  // Lista de módulos
+  modules = [
+    { name: 'Módulo 1 Básico I', progress: 100 },
+    { name: 'Módulo 2 Básico II', progress: 33 },
+    { name: 'Módulo 3 Intermediário I', progress: 0 },
+    { name: 'Módulo 4 Intermediário II', progress: 0 },
+    { name: 'Módulo 5 Avançado', progress: 0 }
+  ];
+
+  // Alterna a visibilidade dos módulos
+  toggleModules(): void {
+    this.showModules = !this.showModules;
+  }
+}
+
