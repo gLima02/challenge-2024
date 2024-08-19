a1d390ce96c35772e377f7079ee59e9dda045dab
MaCintr <marcelocintra2307@gmail.com>
2024-08-18 23:33:18 -0300

    Tela de Boas-Vindas + Animação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TelaLoginComponent } from './components/tela-login/tela-login.component';
+import { WellcomePageComponent } from './components/wellcome-page/wellcome-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: TelaLoginComponent },
+    { path: '', component: TelaLoginComponent },
+    { path: 'welcome', component: WellcomePageComponent },
+];
--- src/app/components/tela-login/tela-login.component.ts
+++ src/app/components/tela-login/tela-login.component.ts
@@ -1,4 +1,4 @@
-import { Component,  } from '@angular/core';
+import { Component, } from '@angular/core';
 
 @Component({
   selector: 'app-tela-login',
@@ -8,5 +8,29 @@ import { Component,  } from '@angular/core';
   styleUrl: './tela-login.component.css'
 })
 export class TelaLoginComponent {
+  onLoginClick(event: Event): void {
+    event.preventDefault(); // Impede o comportamento padrão do formulário
+    
+    // Adiciona as classes de animação aos elementos
+    const eurofarmaImg = document.querySelector('#eurofarma') as HTMLElement;
+    const loginCard = document.querySelector('.card') as HTMLElement;
+    const welcomeText = document.querySelector('.welcome-text') as HTMLElement;
 
+    if (eurofarmaImg) {
+      eurofarmaImg.classList.add('animate-slide-left');
+    }
+
+    if (loginCard) {
+      loginCard.classList.add('animate-fade-out');
+    }
+
+    if (welcomeText) {
+      welcomeText.classList.add('animate-slide-in-right');
+    }
+
+    // Redirecionar ou executar outra lógica após a animação, se necessário
+    setTimeout(() => {
+      // Por exemplo, redirecionar para uma nova página ou mostrar uma nova view
+    }, 1000); // Tempo deve coincidir com a duração das animações
+  }
 }
