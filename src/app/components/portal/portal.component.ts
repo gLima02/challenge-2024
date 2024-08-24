5ccc6be0a13adcbe5b7e852ed489a544b4c2782c
MaCintr <marcelocintra2307@gmail.com>
2024-08-23 22:42:07 -0300

    Merge + Style + Error message

--- src/app/components/portal/portal.component.ts
+++ src/app/components/portal/portal.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { ModuloComponent } from '../modulo/modulo.component';
 import { CursoComponent } from "../curso/curso.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-portal',
   standalone: true,
-  imports: [CommonModule, NavBarComponent, ModuloComponent, CursoComponent],
+  imports: [CommonModule, NavBarComponent, ModuloComponent, CursoComponent, RouterModule],
   templateUrl: './portal.component.html',
   styleUrls: ['./portal.component.css'] // Corrigido de 'styleUrl' para 'styleUrls'
 })
--- src/app/components/tela-login/tela-login.component.ts
+++ src/app/components/tela-login/tela-login.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/login-service.service';
 import { CommonModule, NgFor } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { routes } from '../../app.routes';
 @Component({
   selector: 'app-tela-login',
   standalone: true,
-  imports: [NgFor, CommonModule],
+  imports: [NgFor, CommonModule, RouterModule],
   templateUrl: './tela-login.component.html',
   styleUrl: './tela-login.component.css'
 })
 export class TelaLoginComponent implements OnInit {
   users: any[] = [];
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
     this.userService.getUsers().subscribe(
@@ -31,11 +33,50 @@ export class TelaLoginComponent implements OnInit {
     if (user) {
       if (user.role === 'admin') {
         console.log('login admin');
+
       } else if (user.role === 'collaborator') {
         console.log('login collaborator');
+
+      }
+
+      if (user.role === 'admin' || user.role === 'collaborator') {
+        const alertDiv = document.querySelector('#alert-div') as HTMLElement
+        alertDiv.remove()
+        const fullName = user.fullName
+        console.log(fullName)
+        const eurofarmaImg = document.querySelector('#eurofarma') as HTMLElement;
+        const loginCard = document.querySelector('.card') as HTMLElement;
+        const welcomeText = document.querySelector('.welcome-text') as HTMLElement;
+        welcomeText.innerHTML = `Olá, ${fullName}!<br />Bem-Vindo ao<br />novo OnBoarding!`
+        const startBtn = document.querySelector('#start') as HTMLElement;
+
+        // Adiciona as classes de animação aos elementos
+        eurofarmaImg.classList.add('animate-slide-left');
+        loginCard.classList.add('animate-fade-out');
+        welcomeText.classList.add('animate-slide-in-right');
+
+
+        // Redirecionar ou executar outra lógica após a animação, se necessário
+        setTimeout(() => {
+          startBtn.classList.add('animate-start')
+          // Por exemplo, redirecionar para uma nova página ou mostrar uma nova view
+        }, 2000); // Tempo deve coincidir com a duração das animações
       }
     } else {
-      console.log('Usuário ou senha inválidos');
+      const alertDiv = document.querySelector('#alert-div') as HTMLElement
+      const inputPassword = document.querySelector('#pass-div') as HTMLElement
+      // inputPassword.innerHTML = ` <label for="password" class="form-label">Senha</label>
+      //     <input
+      //       type="password"
+      //       class="form-control"
+      //       id="password"
+      //       placeholder="Digite sua senha"
+      //       required
+      //     />`
+      alertDiv.innerHTML = `<div class="alert alert-danger" role="alert">
+ Erro: Email ou senha inválidos
+</div>`
+      console.log('Erro: Usuário ou senha inválidos');
     }
   }
 }
