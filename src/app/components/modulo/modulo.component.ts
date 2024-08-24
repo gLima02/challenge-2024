3f976303c2bccd0e9161424bfc4b2515ce42da68
gLima <guilherme.l.oliveira2011@gmail.com>
2024-08-24 00:07:29 -0300

    FIX: import ts/redirecionamento curso

--- src/app/components/modulo/modulo.component.ts
+++ src/app/components/modulo/modulo.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { CursoComponent } from '../curso/curso.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-modulo',
   standalone: true,
-  imports: [CommonModule, CursoComponent],
+  imports: [CommonModule, CursoComponent, RouterModule],
   templateUrl: './modulo.component.html',
   styleUrls: ['./modulo.component.css']
 })
