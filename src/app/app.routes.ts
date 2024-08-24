3c32bba50084c56700d4c6a8cd35ef981677e697
gLima <guilherme.l.oliveira2011@gmail.com>
2024-08-23 23:35:33 -0300

    FIX: redirecionamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { PortalComponent } from './components/portal/portal.component';
 import { TelaLoginComponent } from './components/tela-login/tela-login.component';
 
 export const routes: Routes = [
-  {path: '#', component: TelaLoginComponent},
+  {path: '', component: TelaLoginComponent},
   {path: 'portal', component: PortalComponent},
-  {path: 'curso-atv', component: CursoComponent}
+  {path: 'curso-atv', component: CursoComponent},
+  {path: '#', component: TelaLoginComponent}
 ];
