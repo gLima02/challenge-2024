a2e0fd34896e982bdb068deae0d5f583b80c71a6
MaCintr <marcelocintra2307@gmail.com>
2024-08-25 18:40:30 -0300

    FIX: links funcionais - barra de rolagem normal / tela de login inacabada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { CursoComponent } from './components/curso/curso.component';
 import { PortalComponent } from './components/portal/portal.component';
 import { TelaLoginComponent } from './components/tela-login/tela-login.component';
+import { ProgressComponent } from './components/progress/progress.component';
 
 export const routes: Routes = [
   {path: '', component: TelaLoginComponent},
   {path: 'portal', component: PortalComponent},
   {path: 'curso-atv', component: CursoComponent},
-  {path: '#', component: TelaLoginComponent}
+  {path: '#', component: TelaLoginComponent},
+  {path: 'progresso', component: ProgressComponent},
+
 ];
