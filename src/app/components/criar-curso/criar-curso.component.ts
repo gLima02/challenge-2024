cea03f41310e2ea4d56659ed9da439c9b71128e7
MaCintr <marcelocintra2307@gmail.com>
2024-09-27 21:30:15 -0300

    Tela de criação de curso criada

---
+++ src/app/components/criar-curso/criar-curso.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'app-criar-curso',
+  standalone: true,
+  imports: [NavBarComponent],
+  templateUrl: './criar-curso.component.html',
+  styleUrl: './criar-curso.component.css'
+})
+export class CriarCursoComponent {
+
+}
